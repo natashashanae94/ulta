08e04cc5bb72da8676535805fa3519b2851b9773
Natasha Johnson <83310858+natashaJ-NZP@users.noreply.github.com>
2022-04-18 20:07:36 -0400

    created the divs to the clockBG

--- src/components/Clock/ClockBG.jsx
+++ src/components/Clock/ClockBG.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import '../../assets/css/_circlebar.scss';
 
 const ClockBG = ({bg}) => { 
+
+    let url = "";
     return(
         <>
             <div class="projects__panel projects__panel__left"></div>
@@ -11,7 +13,26 @@ const ClockBG = ({bg}) => {
 
             {bg.map((hour, index) => ( 
                 <React.Fragment key={index}>
-                    <article class="project w-xs-100 h-xs-100 clearfix" style={{ backgroundImage: `url("${hour.bg}")`}}></article>
+                    <article className="project w-xs-100 h-xs-100 clearfix" style={{ backgroundImage: `url("${hour.bg}")`}}>
+                        <div className="project__panel project__content w-xs-50 h-xs-100 background--maison-grimaud color-white">
+                            <div className="project__content__container display-xs-table w-xs-200 w-sm-100 h-xs-100">
+                                <div className="display-xs-table-cell h-xs-100 valign-xs-middle valign-sm-bottom">
+                                    <div className="project__content__inner display-md-inline-block text-xs-center text-sm-left">
+                                        <div className="clock text-xs-center color-white project__clock visible-xs-block">
+                                            <time className="clock__display">
+                                                <div className="clock__display__hours"></div>
+                                                <div className="clock__display__separator"></div>
+                                                <div className="clock__display__minutes"></div>
+                                            </time>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href={url}>
+                            <div className="project__panel project__thumbnail"></div>
+                        </a>
+                    </article>
                 </React.Fragment>
             ))}
         </>
