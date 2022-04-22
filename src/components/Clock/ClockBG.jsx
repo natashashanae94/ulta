b9a6381f03a780944024806dc16e508a7c66ef36
Natasha Johnson <83310858+natashaJ-NZP@users.noreply.github.com>
2022-04-21 20:27:32 -0400

    first commit today. added bg colors

--- src/components/Clock/ClockBG.jsx
+++ src/components/Clock/ClockBG.jsx
@@ -7,15 +7,14 @@ const ClockBG = ({bg}) => {
         <>
             <div className="projects__panel projects__panel__left"></div>
             <div className="projects__panel projects__panel__right"></div>
+            <div className="projects">
 
-            {bg.map((hour, index) => {
-                const image = `${process.env.PUBLIC_URL}${hour.bg}`;
-                return (
-                <>
-                    <div className="projects">
+                {bg.map((hour, index) => {
+                    return (
+                        
                     <React.Fragment key={index}>
-                    <article className="project w-xs-100 h-xs-100 clearfix">
-                            <div className="project__panel project__content w-xs-50 h-xs-100 background--maison-grimaud color-white">
+                        <article className="project w-xs-100 h-xs-100 clearfix">
+                            <div className="project__panel project__content w-xs-50 h-xs-100 color-white" style={{ backgroundColor: `${hour.bgColor}` }}>
                                 <div className="project__content__container display-xs-table w-xs-200 w-sm-100 h-xs-100">
                                     <div className="display-xs-table-cell h-xs-100 valign-xs-middle valign-sm-bottom">
                                         <div className="project__content__inner display-md-inline-block text-xs-center text-sm-left">
@@ -32,15 +31,15 @@ const ClockBG = ({bg}) => {
                             </div>
                             <div className="project__panel project__thumbnail w-xs-50 h-xs-100 overflow-hidden">
                                 <a href={url}>
-                                    <div className="project__thumbnail__image w-xs-100 h-xs-100 background-cover lazypreload lazyloaded" style={{ backgroundImage: `url("${hour.bg}")`}}></div>
+                                    <div className="project__thumbnail__image w-xs-100 h-xs-100 background-cover lazypreload lazyloaded" style={{ backgroundImage: `url("${process.env.PUBLIC_URL}${hour.bg}")`}}></div>
                                 </a>
                             </div>
                         </article>
                     </React.Fragment>
-                    </div>
-                </>
-                )
-            })}
+                    );
+                })}
+
+            </div>
         </>
     );
 }
